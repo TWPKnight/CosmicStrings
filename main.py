9620e9f687576eb65cb885f7d796456fd603195a
Tom <zavc888@live.rhul.ac.uk>
2017-10-18 00:05:08 +0100

    Neaten
    Added a header-comment with the course code, authors, start date, etc.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-print "This is the cosmic string project!"
-print "Made this change on a new branch called TestBranch"
+"""
+RHUL PH4100 - Major Project
+Cosmic Strings
+Started: 14/10/2017
+Tom Hyatt & Virginia d'Emilio
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 from random import randint 
+
 N = 10
 box = np.zeros((N,N,N))
 for i in range(len(box[:,0,0])):
