2df75b5d169c28f05ac9db816e2703142cbd142c
Tom <zavc888@live.rhul.ac.uk>
2017-10-18 00:15:10 +0100

    Added_Class
    A class and object have been added. So now the box array is created in its constructor.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 RHUL PH4100 - Major Project
 Cosmic Strings
 Started: 14/10/2017
-Tom Hyatt & Virginia d'Emilio
+Thomas Hyatt & Virginia d'Emilio
 """
 
 import numpy as np
@@ -10,10 +10,22 @@ import matplotlib.pyplot as plt
 import math
 from random import randint 
 
+class SpaceCube:
+
+    def __init__(self, N):
+        """
+        Constructor that creates a cubic lattice (NxNxN) and 
+        assigns a random number (0, 1 or 2) to each point
+        """
+        box = np.zeros((N,N,N))
+        for i in range(len(box[:,0,0])):
+            for j in range(len(box[0,:,0])):
+                for k in range(len(box[0,0,:])):
+                    box[i,j,k] = randint(0,2)
+        self.box=box
+
+
+
 N = 10
-box = np.zeros((N,N,N))
-for i in range(len(box[:,0,0])):
-    for j in range(len(box[0,:,0])):
-        for k in range(len(box[0,0,:])):
-            box[i,j,k] = randint(0,2)
-print box
+lattice = SpaceCube(N)
+print lattice.box
