1b5d8f044aa46ade31d4a984efd6840b9cc372e7
virginiademi <32883514+virginiademi@users.noreply.github.com>
2017-10-17 22:00:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print "This is the cosmic string project!"
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from random import randint 
+N = 10
+box = np.zeros((N,N,N))
+for i in range(len(box[:,0,0])):
+    for j in range(len(box[0,:,0])):
+        for k in range(len(box[0,0,:])):
+            box[i,j,k] = randint(0,2)
