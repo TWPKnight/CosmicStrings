bc7e668f59abfc77eb93901d6fff292221e6370f
virginiademi <32883514+virginiademi@users.noreply.github.com>
2018-01-22 13:45:27 +0000

    Attempt at density plot
    Perimeter seems to be working ok
    Problems with the fitting

--- main.py
+++ main.py
@@ -863,25 +863,40 @@ A[:,0] += lattice.length_loop
 A[:,1] += lattice.size_loop
 i = 0
 for i in xrange(5, max(lattice.size_loop)):
-#for i in xrange(5, 33):
     select = (A[:,1] == i)
     if len(A[select,0])!=0:
         #if(len(A[select,0])>4):
             Avg_L.append(sum(A[select,0])/len(A[select,0]))
             P_Range.append(A[select,1][0])
             
-
 plt.figure("Fig.4")
 plt.scatter(np.log10(P_Range), np.log10(Avg_L))
 popt2,pcov2 = curve_fit(func, P_Range, Avg_L)
 plt.plot( np.log10(P_Range), np.log10(func(P_Range,*popt2)) , c = 'blue')
 plt.xlabel(r'$Log(Loop \ Perimeter)$', size = '16')
-plt.ylabel(r'$Log(\< Length \>)$', size = '16')
+plt.ylabel(r'$Log(< Length >)$', size = '16')
 plt.title(r'$Estimation \ of \ the \ fractal \ dimension$', size = '16')
 #plt.annotate("$l = AR^d$ \n $A = %0.3f \pm %0.3f$ \n $d = %0.3f \pm %0.3f$" %(popt[0],error[0],popt[1], error[1]), xy = (1.5,0.5), size = '16')
 plt.show("Fig.4")
 print "Fit Params: ",popt2
 print "Cor Martix: ",np.sqrt(np.diag(pcov2))
 
-
-
+n = []
+for i in xrange(5, max(lattice.size_loop)):
+    select = (A[:,1] == i)
+    if len(A[select,1])!=0:
+        n.append(1.0*len(A[select,1])/(N**3))
+        
+def func_n(B, n, beta):
+    return (n/B)**(1./beta)
+#def func_n(B, R, beta):
+#    return B/(R**(beta))
+plt.figure("Fig.5")
+plt.scatter(np.log10(P_Range), np.log10(n))
+popt3,pcov3 = curve_fit(func_n, P_Range, n)
+plt.plot( np.log10(P_Range), np.log10(func_n(P_Range,*popt3)) , c = 'blue')
+plt.xlabel(r'$Log(Loop \ Perimeter)$', size = '16')
+plt.ylabel(r'$Log(Density)$', size = '16')
+plt.show("Fig.5")
+print "Fit Params: ",popt3
+print "Cor Martix: ",np.sqrt(np.diag(pcov3))
